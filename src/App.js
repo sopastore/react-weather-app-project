a9ba499a4b5e076ac83bcf26bbac8656ae290a5c
sopastore <pastore.sof@gmail.com>
2022-11-29 16:39:09 -0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This proyect was coded by{" "}
-        <a href="https://phenomenal-alpaca-b8ef22.netlify.app/" target="_blank">
-          Sofia Pastore
-        </a>
-        , and it is{" "}
-        <a
-          href="https://github.com/sopastore/react-weather-app-project"
-          target="_blank"
-        >
-          open-source
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <p>
+          This proyect was coded by{" "}
+          <a
+            href="https://phenomenal-alpaca-b8ef22.netlify.app/"
+            target="_blank"
+          >
+            Sofia Pastore
+          </a>
+          , and it is{" "}
+          <a
+            href="https://github.com/sopastore/react-weather-app-project"
+            target="_blank"
+          >
+            open-source
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
