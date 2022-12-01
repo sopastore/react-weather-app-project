7447276983c6144ee69ff2e9d191b81236df7edc
sopastore <pastore.sof@gmail.com>
2022-12-01 18:10:57 -0300

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
-        <p>
+        <footer>
           This proyect was coded by{" "}
           <a
             href="https://phenomenal-alpaca-b8ef22.netlify.app/"
@@ -21,7 +21,7 @@ export default function App() {
           >
             open-source
           </a>
-        </p>
+        </footer>
       </div>
     </div>
   );
